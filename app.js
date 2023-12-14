3b7c966d7af3d882b93e2e1303f2fb734fe72b4c
Gareisignacio <93623301+Gareisignacio@users.noreply.github.com>
2023-12-14 19:41:40 -0300

    json

---
+++ app.js
@@ -0,0 +1,6 @@
+const scoreNum = document.querySelector(".score-num");
+const scoreDiv = document.querySelector("#scores");
+const iconCat = document.querySelector("#icon");
+const category = document.querySelector("#category");
+const catNum = document.querySelector("#category-score")
+
